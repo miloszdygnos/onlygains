590dd82f1b0d4acd5a3d3c7b6aa8c0be1a0c1af0
milodygon <65972982+miloszdygnos@users.noreply.github.com>
2024-06-25 16:33:22 +0200

    Update Blog.jsx

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -11,7 +11,7 @@ export default function Blog({blogData, handleDelete, userName}){
             <h1 className="blogTitle logo">{blogData.title}</h1>
             <p className="blogContent">{blogData.content}</p>
             <div className="userInfo">
-                <div className="userName">@{blogData.author.name}, {`${day}-${month < 10 ? "0" + month : month }-${year}`} at {`${hours}:${minutes}`}</div>
+                <div className="userName">@{blogData.author.name}, {`${day}-${month < 10 ? "0" + month : month }-${year}`} at {`${minutes < 10 ? "0" + minutes : minutes}`}</div>
             </div>
             {userName === blogData.author.id ? <button onClick={() => handleDelete(blogData.id)} className="deletePost">delete</button> : ""}
         </div>
